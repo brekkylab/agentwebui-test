ca16d2d2205e402dd778af76f9601a9b97aba35c
selloriwoo <sellossier@gmail.com>
2026-04-24 20:11:53 +0900

    feat: add convert_pdf_to_md by pyinstaller and shell

---
+++ chat-agent/convert-pdf-to-md/build.sh
@@ -0,0 +1,16 @@
+rm -f run.spec
+pyinstaller run.py \
+    --onedir \
+    --noconfirm \
+    --recursive-copy-metadata=docling \
+    --collect-all=docling \
+    --collect-all=docling_core \
+    --collect-all=docling_ibm_models \
+    --collect-all=docling_parse \
+    --exclude-module=hf_xet \
+    --exclude-module=faker \
+    --exclude-module=tree_sitter \
+    --exclude-module=tree_sitter_typescript \
+    --exclude-module=tree_sitter_c \
+    --exclude-module=tree_sitter_javascript \
+    --exclude-module=tree_sitter_python
